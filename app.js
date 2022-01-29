62ec6aaf153d776d564e8fd01e9a3057700729b1
nurcanyldz <47923561+nurcanyldz@users.noreply.github.com>
2022-01-29 21:57:52 +0200

    saved the files

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-window.addEventListener('keydown',  function(e) {
-console.log(e);
+window.addEventListener('keydown', function(e) {
+  console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(audio);
+
+  if (!audio) {
+    return; // stop the funtion.
+  }
+
+  audio.currentTime = 0;
+  audio.play();
 });
+
+
